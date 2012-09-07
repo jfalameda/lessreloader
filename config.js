27269d4cb6059aeb3f2692e74f9134db7d3f7b34
José Fernández Alameda <jose.fernandez.alameda@gmail.com>
2012-09-07 16:32:37 +0200

    Bug fixing

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	port: 8181, // Server port
 	serverLogLevel: 1, // Server verbose level
-	paths: ['../../less/spl'], // LESS files path
+	paths: ['less'], // LESS files path
 	
 	//Files to be compiled and the route for the out CSS
 	lessFiles: [
-		{ src: "../../less/spl/spl.less", css: "../../css/compiled/spl.css", ready: false },
-		{ src: "../../less/spl/ie7.less", css: "../../css/compiled/ie7.css", ready: false }
+		{ src: "less/main.less", css: "main.css", ready: false },
+		{ src: "less/ie7.less", css: "ie7.css", ready: false }
 	]
 };
